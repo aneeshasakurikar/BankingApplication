16654353c5a035d302edfc24d7bc96e1b5c61afd
SesivardhanReddy <SESIVARDHANREDDY.C@lntinfotech.com>
2021-01-13 06:45:05 +0530

    app routing

--- FrontendNew/src/app/app-routing.module.ts
+++ FrontendNew/src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
 import { ForgotUserIdComponent } from './forgot-user-id/forgot-user-id.component';
+import { LoginPageComponent } from './login-page/login-page.component';
 import { NetbankingRegistrationComponent } from './netbanking-registration/netbanking-registration.component';
 import { RegistrationPageComponent } from './registration-page/registration-page.component';
 import { SetNewPasswordComponent } from './set-new-password/set-new-password.component';
@@ -10,7 +11,7 @@ import { SetNewPasswordComponent } from './set-new-password/set-new-password.com
 
 const routes: Routes = [
   // {path:'set-pass',component:SetNewPasswordComponent}
-  {path:'', component:AppComponent},
+  {path:'', component:LoginPageComponent},
   { path:'register', component:RegistrationPageComponent },
   { path:'forgotId',component:ForgotUserIdComponent},
   { path:'forgotPass',component:ForgotPasswordComponent}
